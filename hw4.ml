f6b4afcab0a2143c5b89529021b477f6d0fe5be7
Sanket Maskey <smaske@cs.washington.edu>
2025-07-30 11:18:45 -0700

    Problem 1-5

--- hw4.ml
+++ hw4.ml
@@ -2,21 +2,69 @@ open Hw4types
 
 (* 1 *)
 let rec sequence spacing low high : int list =
-  failwith "sequence: not implemented"
+  let rec go current =
+    if current > high then
+      []
+    else
+      current :: go (current + spacing)
+  in
+  go low
 
 (* 2 *)
-let string_append_map xs suffix = failwith "string_append_map: not implemented"
+let string_append_map xs suffix =
+  List.map (fun s -> s ^ suffix) xs
 
 (* 3 *)
-let list_nth_mod xs n = failwith "list_nth_mod: not implemented"
+let list_nth_mod xs n =
+  (* First, guard against negative inputs as required. *)
+  if n < 0 then
+    raise Negative_Input
+  else
+    match xs with
+    | [] -> raise EmptyList
+    | _ ->
+        (* Get its length. *)
+        let len = List.length xs in
+        (* Calculate the index using the modulo operator. *)
+        let index = n mod len in
+        (* Return the element at the calculated index. *)
+        List.nth xs index
 
 (* 4 *)
 let rec stream_first_k_such_that p k (Stream t) =
-  failwith "stream_first_k_such_that: not implemented"
+  if k = 0 then
+    []
+  else
+    match t () with
+    (* If the stream is empty, we can't get any more elements. *)
+    | None ->
+        (* As k is not 0 here, we haven't found enough elements, so raise an exception. *)
+        raise EmptyStream
+    (* If the stream has a value 'v' and a 'next_s' stream. *)
+    | Some (v, next_s) ->
+        (* Check if the value 'v' satisfies the predicate 'p'. *)
+        if p v then
+          (* If it does, add 'v' to our result list and recursively find the remaining k-1 elements. *)
+          v :: stream_first_k_such_that p (k - 1) next_s
+        else
+          (* If not, discard 'v' and recursively look for k elements in the rest of the stream. *)
+          stream_first_k_such_that p k next_s
 
 (* 5 *)
 let funny_number_stream : int stream =
-  Stream (fun () -> failwith "funny_number_stream: not implemented")
+  (* Define a recursive helper function 'go' that takes the current number 'n'
+   * and generates the stream from that point onward. *)
+  let rec go n =
+    Stream
+      (fun () ->
+        (* Determine the value: if n is divisible by 6, negate it; otherwise, use n. *)
+        let v = if n mod 6 = 0 then -n else n in
+        (* The stream produces the value 'v' and a thunk for the next part of the stream,
+         * which is a recursive call to 'go' with n+1. *)
+        Some (v, go (n + 1)))
+  in
+  (* The stream starts by calling our generator with the number 1. *)
+  go 1
 
 (* 6 *)
 let foo_then_bar : string stream =
--- hw4test.ml
+++ hw4test.ml
@@ -1,14 +1,80 @@
 open Hw4lib
 open Hw4
 
-(* 1 *)
+(* Problem 1 *)
 let%test _ = sequence 2 3 11 = [3; 5; 7; 9; 11]
 let%test _ = sequence 3 3 8 = [3; 6]
 let%test _ = sequence 1 3 2 = []
+let%test _ = sequence 10 5 5 = [5] (* Low and high are the same. *)
+let%test _ = sequence 10 0 9 = [0] (* Next number (0 + 10) would exceed high, so the list should only contain the starting number. *)
 
-(* 2 *)
+(* Problem 2 *)
 let%test _ = string_append_map [] "foo" = []
-(* more tests here *)
+let%test _ = string_append_map ["a"; "b"; "c"] "!" = ["a!"; "b!"; "c!"]
+let%test _ = string_append_map ["hello"] " world" = ["hello world"]
+let%test _ = string_append_map ["ocaml"; "is"; "fun"] "" = ["ocaml"; "is"; "fun"]
+let%test _ = string_append_map [""; ""] "test" = ["test"; "test"]
+
+(* Problem 3 *)
+let%test _ = list_nth_mod [10; 20; 30] 1 = 20
+let%test _ = list_nth_mod ["a"; "b"] 3 = "b" (* 3 mod 2 = 1, which is "b" *)
+let%test _ = list_nth_mod [5] 100 = 5 (* 100 mod 1 = 0, which is 5 *)
+
+let%test _ =
+  try
+    let _ = list_nth_mod [] 5 in
+    false (* Fail if EmptyList is not raised *)
+  with
+  | EmptyList -> true (* Pass if EmptyList is raised *)
+
+let%test _ =
+  try
+    let _ = list_nth_mod [1; 2] (-1) in
+    false (* Fail if Negative_Input is not raised *)
+  with
+  | Negative_Input -> true (* Pass if Negative_Input is raised *)
+
+(* Problem 4 *)
+
+(* Helper function to create a finite stream from a list for testing. *)
+let stream_of_list xs =
+  let r = ref xs in
+  Stream
+    (fun () ->
+      match !r with
+      | [] -> None
+      | h :: t ->
+          r := t;
+          Some (h, stream_of_list !r))
+
+(* A simple infinite stream of natural numbers for testing. *)
+let rec from n = Stream (fun () -> Some (n, from (n + 1)))
+
+let%test _ =
+  let is_even n = n mod 2 = 0 in
+  stream_first_k_such_that is_even 3 (from 1) = [2; 4; 6]
+
+let%test _ =
+  stream_first_k_such_that (fun _ -> true) 0 (from 1) = []
+
+let%test _ =
+  let s = stream_of_list [1; 2; 3; 4; 5] in
+  stream_first_k_such_that (fun x -> x > 0) 5 s = [1; 2; 3; 4; 5]
+
+let%test _ =
+  try
+    let s = stream_of_list [2; 4] in
+    let _ = stream_first_k_such_that (fun x -> x mod 2 = 0) 3 s in
+    false (* Fail if EmptyStream is not raised *)
+  with
+  | EmptyStream -> true (* Pass if EmptyStream is raised *)
 
 
+(* Problem 5 *)
+let%test _ =
+  stream_first_k_such_that (fun _ -> true) 7 funny_number_stream
+  = [1; 2; 3; 4; 5; -6; 7]
 
+let%test _ =
+  stream_first_k_such_that (fun _ -> true) 13 funny_number_stream
+  = [1; 2; 3; 4; 5; -6; 7; 8; 9; 10; 11; -12; 13]
